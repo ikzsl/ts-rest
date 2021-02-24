0a3bb53a1d362b153b4095c6140f4f05bb74ed5e
Muslim <ikzsl@yandex.ru>
2021-02-24 09:10:39 +0300

    add miragejs server

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,47 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { createServer } from "miragejs";
+
+
+type UserType = {
+  sayHello?: Function
+  name: string
+  age: number
+  isSamurai?: boolean
+  id: string
+}
+
+createServer({
+  routes() {
+    this.get("/api/users", () => [
+      { id: "1", name: "Luke" },
+      { id: "2", name: "Leia" },
+      { id: "3", name: "Anakin" },
+    ])
+  },
+})
+
+
+
 
 function App() {
+  let [users, setUsers] = useState([])
+
+  useEffect(() => {
+    fetch("/api/users")
+      .then((response) => response.json())
+      .then((json) => setUsers(json))
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <ul>
+      {users.map((user: UserType) => (
+        <li key={user.id}>{user.name}</li>
+      ))}
+    </ul>
+  )
 }
 
 export default App;
+
