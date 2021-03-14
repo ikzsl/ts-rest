d616dc124658030c8f00be7c449e4b3380f72383
Muslim <ikzsl@yandex.ru>
2021-03-15 02:29:03 +0300

    add server

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,10 @@
 import React, { useEffect, useState } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import { createServer } from "miragejs";
-
 
 type UserType = {
-  sayHello?: Function
   name: string
-  age: number
-  isSamurai?: boolean
   id: string
 }
-
-createServer({
-  routes() {
-    this.get("/api/users", () => [
-      { id: "1", name: "Luke" },
-      { id: "2", name: "Leia" },
-      { id: "3", name: "Anakin" },
-    ])
-  },
-})
-
-
 
 
 function App() {
@@ -32,15 +14,11 @@ function App() {
     fetch("/api/users")
       .then((response) => response.json())
       .then((json) => setUsers(json))
-  }, [])
+  }, []);
 
-  return (
-    <ul>
-      {users.map((user: UserType) => (
-        <li key={user.id}>{user.name}</li>
-      ))}
-    </ul>
-  )
+
+
+  return (<p>{`${users}`}</p>)
 }
 
 export default App;
