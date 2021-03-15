7fbc49a5c763f86c40cf8e4f9a3ae3926841cefc
Muslim <ikzsl@yandex.ru>
2021-03-15 04:26:57 +0300

    add controllers

--- server/index.js
+++ server/index.js
@@ -1,8 +1,13 @@
 import express from 'express';
+import usersRoutes from './routes/users.js';
 
 const app = express();
 const PORT = 5000;
 
 app.use(express.json({ extended: true }));
+
+app.use('/users', usersRoutes)
+
+app.get('/', (req, res) => res.send('Hello from Homepage.'));
 
 app.listen(PORT, () => console.log(`Server running on port: http://localhost:${PORT}`));
